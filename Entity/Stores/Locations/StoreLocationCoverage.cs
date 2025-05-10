e10d4ae444dab4e27b985ca8d6bdfaceeec2e8e9
Onur Eymen <onurceymen@yahoo.com>
2025-05-10 08:57:26 +0300

    Location servisi için daha hızlı bi yapı kurulacak

---
+++ Entity/Stores/Locations/StoreLocationCoverage.cs
@@ -0,0 +1,17 @@
+﻿namespace Entity.Stores.Locations
+{
+    public class StoreLocationCoverage
+    {
+        public int Id { get; set; }
+        public int StoreId { get; set; }
+
+        public HashSet<int> RegionIds { get; set; } = new();
+        public HashSet<int> CountryIds { get; set; } = new();
+        public HashSet<int> StateIds { get; set; } = new();
+        public HashSet<int> ProvinceIds { get; set; } = new();
+        public HashSet<int> DistrictIds { get; set; } = new();
+        public HashSet<int> NeighborhoodIds { get; set; } = new();
+
+        public DateTime LastUpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
