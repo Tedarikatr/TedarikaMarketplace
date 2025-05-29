dd2b20a46bfa33eef62625b3a5d25ee8273fa8b2
Onur Eymen <117844492+onurceymen@users.noreply.github.com>
2025-05-29 12:26:56 +0300

    Bazı Revizeler

---
+++ Entity/OrderOffers/QuotationRequest.cs
@@ -0,0 +1,29 @@
+﻿using Entity.Auths;
+using Entity.Stores.Products;
+
+namespace Entity.OrderOffers
+{
+    public class QuotationRequest
+    {
+        public int Id { get; set; }
+        public int BuyerUserId { get; set; }
+        public BuyerUser BuyerUser { get; set; }
+
+        public int StoreProductId { get; set; }
+        public StoreProduct StoreProduct { get; set; }
+
+        public int Quantity { get; set; }
+        public string Message { get; set; }
+        public DateTime RequestedAt { get; set; }
+        public QuotationRequestStatus Status { get; set; }
+
+        public ICollection<QuotationResponse> Responses { get; set; }
+    }
+
+    public enum QuotationRequestStatus
+    {
+        Pending = 0,
+        Responded = 1,
+        Cancelled = 2
+    }
+}
---
+++ Entity/OrderOffers/QuotationResponse.cs
@@ -0,0 +1,22 @@
+﻿using Entity.Auths;
+
+namespace Entity.OrderOffers
+{
+    public class QuotationResponse
+    {
+        public int Id { get; set; }
+
+        public int QuotationRequestId { get; set; }
+        public QuotationRequest QuotationRequest { get; set; }
+
+        public int SellerUserId { get; set; }
+        public SellerUser SellerUser { get; set; }
+
+        public decimal OfferedUnitPrice { get; set; }
+        public int MinOrderQuantity { get; set; }
+        public DateTime ValidUntil { get; set; }
+
+        public string Notes { get; set; }
+        public DateTime RespondedAt { get; set; }
+    }
+}
